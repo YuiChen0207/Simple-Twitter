fcf4cc658ec351d4a3e1796e80a60d5ef890f746
kaihsu12 <kaihsu12@gmail.com>
2023-06-07 14:19:19 +0800

    create AuthInput component

---
+++ src/cpmponent/AuthInput.jsx
@@ -0,0 +1,16 @@
+const AuthInput = ({ type, label, value, placeholder, onChange }) => {
+  return (
+    <div className={}>
+    <div className={}>{label}</div>
+    <input
+      className={}
+      type={type || text}
+      value={value}
+      placeholder={placeholder}
+      onChange={}
+    />
+    </div>
+  );
+};
+
+export default AuthInput;
