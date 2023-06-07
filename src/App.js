58c5c10a42516d668871eced1072b0090438b1dd
kaihsu12 <kaihsu12@gmail.com>
2023-06-07 22:36:20 +0800

    add new route to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainPage from "./page/MainPage";
-import "./styles/base.scss"
-import "./styles/reset.scss";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './page/MainPage';
+import LoginPage from './page/LoginPage';
+import './styles/base.scss';
+import './styles/reset.scss';
 
 /* import {LoginPage, MainPage, RegistPage} from './page' */
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className="app">
         <Routes>
           <Route path="mainPage" element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
         </Routes>
         {/* 
         <AuthProvider>
