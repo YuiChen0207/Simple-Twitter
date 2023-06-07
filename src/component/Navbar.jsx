5eac0ba076ae092081159033fa75597cfd18f022
YuiChen0207 <asd0935252359@gmail.com>
2023-06-07 20:45:02 +0800

    feat: complete navbar layout and style

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,29 +1,28 @@
 import React from "react";
+import NavbarLogo from "../assets/logo.svg";
+import NavbarHomeIcon from "../assets/home.svg";
+import NavbarUserIcon from "../assets/userInfo.svg";
+import NavbarSettingIcon from "../assets/setting.svg";
 import "../styles/navbar.scss";
-import logo from "../assets/logo.svg";
-import home from "../assets/home.svg"
-import user from "../assets/userInfo.svg"
-import setting from "../assets/setting.svg";
-
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-logo">
-        <img src={logo} alt="logo" />
+        <img src={NavbarLogo} alt="logo" />
       </div>
       <div className="navbar-info">
         <div className="navbar-item navbar-home">
-          <img src={home} alt="home" />
-          <span>首頁</span>
+          <img src={NavbarHomeIcon} alt="home" />
+          <h5 className="Bold">首頁</h5>
         </div>
         <div className="navbar-item navbar-user">
-          <img src={user} alt="user" />
-          <span>個人資料</span>
+          <img src={NavbarUserIcon} alt="user" />
+          <h5 className="Bold">個人資料</h5>
         </div>
         <div className="navbar-item navbar-setting">
-          <img src={setting} alt="setting" />
-          <span>設定</span>
+          <img src={NavbarSettingIcon} alt="setting" />
+          <h5 className="Bold">設定</h5>
         </div>
         <div className="navbar-item navbar-button">
           <button>推文</button>
