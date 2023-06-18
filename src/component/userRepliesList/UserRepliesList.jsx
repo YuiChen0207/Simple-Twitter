3383b61dc0969a03b4d09647375ceb46539aaa58
YuiChen0207 <asd0935252359@gmail.com>
2023-06-18 20:27:26 +0800

    feat: add red button line on error message

--- src/component/userRepliesList/UserRepliesList.jsx
+++ src/component/userRepliesList/UserRepliesList.jsx
@@ -1,27 +1,13 @@
-import grayLogo from "../../assets/logoGray.svg";
 import UserPageTweet from "../userPageTweet/UserPageTweet";
+import { formatTime } from "../../utils/timeUtils";
 
 const UserRepliesList = ({ replies }) => {
-  function formatTime(timestamp) {
-    const currentTime = new Date();
-    const postTime = new Date(timestamp);
-
-    const diffInMilliseconds = currentTime - postTime;
-    const diffInHours = Math.floor(diffInMilliseconds / (1000 * 60 * 60));
-
-    if (diffInHours < 1) {
-      return "剛剛";
-    } else {
-      return `${diffInHours}小時`;
-    }
-  }
-
   return (
     <div className="tweetsListContainer">
       {replies.map((reply) => (
         <UserPageTweet
           key={reply.id}
-          logo={reply.replyAvatar ?? grayLogo}
+          logo={reply.replyAvatar}
           username={reply.replyName}
           accountName={reply.replyAccount}
           postTime={formatTime(reply.createdAt)}
